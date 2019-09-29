8dcec5ac6b52cb72859822bf8bd2147ce7c13d37
shaoyayu <1181675233@qq.com>
2019-09-29 20:37:47 +0800

    5.0：管理员邮件管理模块

---
+++ src/com/shaoyuayu/web/servlet/root/RootEmaliServlet.java
@@ -0,0 +1,23 @@
+package com.shaoyuayu.web.servlet.root;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 5.0功能，管理员的邮箱管理模块开发
+ */
+
+@WebServlet(value = {"/RootEmaliServlet"})
+public class RootEmaliServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
